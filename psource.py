6d31429877fd54ad716f190b844ef289762793e5
frederikstroem <github@katofln.com>
2023-05-10 13:57:02 +0200

    Update psource to correct version

--- psource.py
+++ psource.py
@@ -6,9 +6,20 @@ import os
 import sys
 from pathlib import Path
 
-def print_files(file_path):
+
+def remove_base_path(file_path):
+    base_paths = [Path('/home/katofln/projects/'), Path('/home/katofln/')]
+    for base_path in base_paths:
+        try:
+            return '/' + str(file_path.relative_to(base_path))
+        except ValueError:
+            pass
+    return str(file_path)
+
+def print_files(file_path, relative_path):
     with open(file_path, "r") as file:
-        print(f"\033[1m{file_path}:\033[0m")
+        # print(f"\033[1m{relative_path}:\033[0m")
+        print(f"$ {relative_path}:")
         print(file.read())
         print()
 
@@ -28,8 +39,8 @@ def main():
         sys.exit(0)
 
     current_directory = Path.cwd()
-    default_blacklist = {".env", ".gitignore", ".gitmodules", ".gitattributes"}
-    blacklisted_dirs = {".git"}
+    default_blacklist = {".env", ".gitignore", ".gitmodules", ".gitattributes", "Cargo.lock"}
+    blacklisted_dirs = {".git", "target", ".pio"}
     whitelist = set(arg for arg in sys.argv[1:] if arg not in ["--help", "-h"])
 
     for root, _, files in os.walk(current_directory):
@@ -38,14 +49,16 @@ def main():
 
         for file in files:
             file_path = Path(root) / file
+            file_path_str = remove_base_path(file_path)
             file_name = file_path.name
 
             if whitelist:
-                if any(whitelisted_file in str(file_path) for whitelisted_file in whitelist):
-                    print_files(file_path)
+                if any(whitelisted_file in file_path_str for whitelisted_file in whitelist):
+                    print_files(file_path, file_path_str)
             else:
                 if file_name not in default_blacklist:
-                    print_files(file_path)
+                    print_files(file_path, file_path_str)
 
 if __name__ == "__main__":
+    main()
     main()
